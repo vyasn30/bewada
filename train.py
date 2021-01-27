c9e4faa6ec30b58bde429eb0e0a6f17398e46623
vyasn30 <vyasn30@outlook.com>
2021-01-27 06:02:29 +0530

    Fixed a bug regarding kernel size in c3

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ class Net(nn.Module):
 
         self.c1 = nn.Conv2d(64,64,kernel_size=3, padding=1)
         self.c2 = nn.Conv2d(64,64,kernel_size=3, padding=1)
-        self.c3 = nn.Conv2d(64,128,kernel_size=3, stride=2)
+        self.c3 = nn.Conv2d(64,128,kernel_size=1, stride=2)
 
         self.d1 = nn.Conv2d(128,128,kernel_size=1)
         self.d2 = nn.Conv2d(128,128,kernel_size=1)
